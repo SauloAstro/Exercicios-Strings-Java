b536eb610808908b2f62951859a5580bc800fea5
SauloAstro <saulo.mozart09@gmail.com>
2023-09-10 10:59:08 -0300

    Adicionando Exercicio 1

---
+++ Exercicio1/TabelaCaracter.java
@@ -0,0 +1,30 @@
+/*
+
+Exibir na tela os caracteres da tabela de códigos ASCII
+
+
+
+ */
+
+
+public class TabelaCaracter{
+	
+	public static void main (String[] args) {
+		
+		char tabela = (int) 1; 
+		 
+		 for(int i = 1; i < 197; i++)
+		 {
+				
+			++tabela; 
+			System.out.print(tabela);
+			  
+			 
+			 
+		 }
+		
+		
+		
+	}
+}
+
